ec40ed92af396e3962f9e5174a0220df3e28f5fc
nonergodic <nonergodic@gmail.com>
2023-03-07 20:50:57 -0800

    solana: prefer building mpl metadata .so from scratch to downloading prebuilt

--- dust-bridging/solana/dependencies/build_mpl_token_metadata.sh
+++ dust-bridging/solana/dependencies/build_mpl_token_metadata.sh
@@ -5,12 +5,6 @@ if [ $? -eq 0 ]; then
   exit 0
 fi
 
-wget -q https://github.com/metaplex-foundation/js/raw/main/programs/mpl_token_metadata.so
-if [ $? -eq 0 ]; then
-  exit 0
-fi
-
-echo "couldn't download prebuilt .so - fetching MPL repo and building from scratch instead"
 git clone https://github.com/metaplex-foundation/metaplex-program-library.git \
   tmp-metaplex > /dev/null 2>&1
 
@@ -20,5 +14,14 @@ cd ..
 
 mv tmp-metaplex/test-programs/mpl_token_metadata.so .
 rm -rf tmp-metaplex
+
+ls mpl_token_metadata.so > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  exit 0
+fi
+
+echo "building MPL from scratch failed - falling back on downloading prebuilt .so"
+echo "WARNING prebuilt .so might be outdated"
+wget -q https://github.com/metaplex-foundation/js/raw/main/programs/mpl_token_metadata.so
 
 exit 0
