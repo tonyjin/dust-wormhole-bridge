2fda65644850600c90734b89db67e2fc03d81226
Yu-SeriousSam <yzhang@degods.com>
2023-04-10 17:15:56 -0700

    feat(nft): replicate the changes made to the staking repo & abstract the common base contract.

--- dust-bridging/evm/src/nft/y00ts.sol
+++ dust-bridging/evm/src/nft/y00ts.sol
@@ -5,7 +5,7 @@ import {BaseWormholeBridgedNft} from "./BaseWormholeBridgedNft.sol";
 import {IWormhole} from "wormhole-solidity/IWormhole.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract DeGods is BaseWormholeBridgedNft {
+contract y00ts is BaseWormholeBridgedNft {
 	constructor(
 		IWormhole wormhole,
 		IERC20 dustToken,
